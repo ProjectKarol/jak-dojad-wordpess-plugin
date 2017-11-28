d2f5e64d2b9fa2d15a3b35702f86399c1ffb5a8a
Karol Szczesny <karolszczesny@outlook.com>
2017-11-28 12:28:38 +0100

    plugin init

---
+++ jak-dojechac.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  Jak dojechac
+Plugin URI:   https://github.com/ProjectKarol/jak-dojad-wordpess-plugin
+Description:  Pokazuje jak dojechac
+Version:      0.1
+Author:       Karol Szczęsny
+Author URI:   www.karolszczesny.com
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  jak-dojechac
+Domain Path:  /languages
+*/
